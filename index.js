b7d18549e8ff8bd6996b304f8e154602a9393950
noemiepham <huynhtieuquy@gmail.com>
2022-04-27 15:30:54 +0200

    up load new projet

---
+++ index.js
@@ -0,0 +1,8 @@
+﻿var Menuburger = document.querySelector('.MenuBurger');
+var Menunav = document.querySelector('.NavMenu');
+
+Menuburger.addEventListener('click', () => {
+    Menunav.classList.toggle('affichageMenu');
+})
+
+
